a2b22a0f56fb12a5432d2c36a2b687421aee266b
Jason <jason_kiesling@student.uml.edu>
2018-11-09 21:08:51 -0500

    Lesson and Course creation and editing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { Admin, Resource } from 'react-admin';
-import { LessonList, LessonEdit } from './lessons';
-import { UserList, UserEdit, UserCreate } from './users';
-import { SnapshotList } from './snapshots';
+import { CourseList, CourseEdit, CourseCreate } from './pages/courses';
+import { LessonList, LessonEdit, LessonCreate } from './pages/lessons';
+import { UserList, UserEdit, UserCreate } from './pages/users';
+import { SnapshotList } from './pages/snapshots';
 //import Icon from '@material-ui/core/Icon';
+import CourseIcon from '@material-ui/icons/List';
 import LessonIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/SupervisedUserCircle';
 import SnapshotIcon from '@material-ui/icons/CameraEnhance'
-import Dashboard from './dashboard';
-import authProvider from './authProvider';
-import dataProvider from './dataProvider';
+import Dashboard from './pages/dashboard';
+import authProvider from './data_connections/authProvider';
+import dataProvider from './data_connections/dataProvider';
 
 const App = () => (
   <Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider}>
-    <Resource name="lessons" list={LessonList} edit={LessonEdit} icon={LessonIcon} />
+    <Resource name="courses" list={CourseList} edit={CourseEdit} create={CourseCreate} icon={CourseIcon} />
+    <Resource name="lessons" list={LessonList} edit={LessonEdit} create={LessonCreate} icon={LessonIcon} />
     <Resource name="users" list={UserList} edit={UserEdit} create={UserCreate} icon={UserIcon} />
     <Resource name="snapshots" list={SnapshotList} icon={SnapshotIcon} />
   </Admin>
