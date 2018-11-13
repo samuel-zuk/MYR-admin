ab2d54d5095c89b0fd41b832d262cb6ce418e521
Jason <jason_kiesling@student.uml.edu>
2018-11-13 15:57:08 -0500

    Filtering

--- src/pages/snapshots.js
+++ src/pages/snapshots.js
@@ -8,11 +8,34 @@ import {
     Responsive,
     SimpleList,
     SimpleShowLayout,
-    Show
+    Show,
+    Filter,
+    BooleanInput,
+    ShowButton,
+    TextInput
 } from 'react-admin';
 
+const SnapshotFilter = (props) => (
+    <Filter {...props}>
+        {/* <TextInput label="Search" source="q" alwaysOn /> */}
+        {/* <ReferenceInput label="User" source="userId" reference="users" allowEmpty>
+            <SelectInput optionText="name" />
+        </ReferenceInput> */}
+        {/* <ReferenceInput label="User" source="user" reference="snapshots" allowEmpty>
+            <SelectInput optionText="user" source="user" />
+        </ReferenceInput> */}
+        {/* <ReferenceInput label="Snapshot User" source="user" reference="snapshots" field="user" >
+            <SelectInput optionText="user" source="user" />
+        </ReferenceInput> */}
+        {/* <SelectInput source="user" choices="user" reference="snapshots" /> */}
+        <TextInput source="user" />
+        <BooleanInput source="error" />
+        {/* <DateInput source="timestamp" /> */}
+    </Filter>
+);
+
 export const SnapshotList = (props) => (
-    <List title="Snapshots" {...props} bulkActionButtons={false}>
+    <List title="Snapshots" {...props} bulkActionButtons={false} filters={<SnapshotFilter />}>
         <Responsive
             small={
                 <SimpleList
@@ -29,6 +52,7 @@ export const SnapshotList = (props) => (
                     <TextField source="text" />
                     <BooleanField source="error" />
                     <DateField source="timestamp" showTime />
+                    <ShowButton />
                 </Datagrid>
             }
         />
