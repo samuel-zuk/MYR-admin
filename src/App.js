a372ffe6c1e2c119489b356a8ffa9d3c6114de76
Keith Valin <keith@mailtux.net>
2020-06-15 10:57:45 -0400

    Added icons for the separate tabs in the sidebar

--- src/App.js
+++ src/App.js
@@ -17,18 +17,22 @@ import { GoogleLoginList, GoogleLoginShow } from './pages/GoogleLogins';
 import CourseIcon from '@material-ui/icons/List';
 import RefExIcon from '@material-ui/icons/Help';
 import UserIcon from '@material-ui/icons/SupervisedUserCircle';
-import SnapshotIcon from '@material-ui/icons/CameraEnhance'
+import SnapshotIcon from '@material-ui/icons/CameraEnhance';
+import CollectionIcon from '@material-ui/icons/Filter';
+import SceneIcon from '@material-ui/icons/Landscape';
+import NotificationIcon from '@material-ui/icons/PriorityHigh';
+import GoogleLoginIcon from '@material-ui/icons/Face';
 
 
 const App = () => (
   <Admin dashboard={Dashboard} loginPage={loginPage} authProvider={authProvider} dataProvider={dataProvider}>
-    <Resource name="scenes" list={SceneList} show={SceneShow} />
-    <Resource name="collections" list={CollectionList} show={CollectionShow} />
+    <Resource name="scenes" list={SceneList} show={SceneShow} icon={SceneIcon} />
+    <Resource name="collections" list={CollectionList} show={CollectionShow} icon={CollectionIcon} />
     <Resource name="courses" list={CourseList} edit={CourseEdit} create={CourseCreate} icon={CourseIcon} />
     <Resource name="referenceExamples" list={ReferenceExamplesList} edit={ReferenceExamplesEdit} create={ReferenceExamplesCreate} icon={RefExIcon} />
     <Resource name="users" list={UserList} edit={UserEdit} create={UserCreate} icon={UserIcon} />
-    <Resource name="googlelogins" list={GoogleLoginList} show={GoogleLoginShow} />
-    <Resource name="notifications" list={NotifList} edit={NotifEdit} create={NotifCreate} />
+    <Resource name="googlelogins" list={GoogleLoginList} show={GoogleLoginShow} icon={GoogleLoginIcon} />
+    <Resource name="notifications" list={NotifList} edit={NotifEdit} create={NotifCreate} icon={NotificationIcon} />
     <Resource name="snapshots" list={SnapshotList} show={SnapshotShow} icon={SnapshotIcon} />
   </Admin>
 );
